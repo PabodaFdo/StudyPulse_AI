8c26b241cedacc319aa47b4bd1a6ad7729052a10
PabodaFdo <pabodafernandowpsf@gmail.com>
2026-06-30 18:59:54 +0530

    Add risk history service methods

--- client/src/services/risk.service.js
+++ client/src/services/risk.service.js
@@ -5,6 +5,24 @@ const predictRisk = async (payload) => {
   return response.data;
 };
 
+const saveRiskHistory = async (payload) => {
+  const response = await api.post('/risk/history', payload);
+  return response.data;
+};
+
+const getRiskHistory = async (subjectId) => {
+  const response = await api.get(`/risk/history/${subjectId}`);
+  return response.data;
+};
+
+const getLatestRiskPrediction = async (subjectId) => {
+  const response = await api.get(`/risk/history/${subjectId}/latest`);
+  return response.data;
+};
+
 export const riskService = {
   predictRisk,
+  saveRiskHistory,
+  getRiskHistory,
+  getLatestRiskPrediction,
 };
