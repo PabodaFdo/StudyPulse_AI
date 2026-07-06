45e9a994303643088bfeafc332d2cdc4adc7742e
PabodaFdo <pabodafernandowpsf@gmail.com>
2026-07-06 12:34:41 +0530

    Add summary review API service

--- client/src/services/summary.service.js
+++ client/src/services/summary.service.js
@@ -1,7 +1,7 @@
 import api from './api';
 
 export const generateSummary = async (text) => {
-  const response = await api.post('/summary/generate', { text });
+  const response = await api.post('/summaries/generate', { text });
   return response.data;
 };
 
